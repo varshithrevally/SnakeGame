39ae662cb4a73ed3db3acefd544afd04bc2cd718
varshithrevally <revallyvarshith989@gmail.com>
2024-08-19 16:05:14 +0530

    Add files via upload

---
+++ SnakeGUI/src/SnakeGame.java
@@ -0,0 +1,11 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         //GameFrame frame=new GameFrame();
+         
+         new GameFrame();
+	}
+
+}
